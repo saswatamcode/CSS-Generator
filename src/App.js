67593743ee5a05e4cbb615974cf23239796f740e
Saswata Mukherjee <saswataminsta@yahoo.com>
2020-05-10 19:58:49 +0530

    Add starter files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Tabs from './components/Tabs';
 
 function App() {
   return (
     <div className="App">
-     <h1>CSS Generator</h1>
+      <Header />
+      <Tabs />
     </div>
   );
 }
---
+++ src/components/Tabs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Tabs() {
+    return(
+        <div className='tabs'>
+            <button>Style a button</button>
+        </div>
+    );
+}
+
+export default Tabs;
